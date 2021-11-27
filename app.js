dbc7eb2a6c4ec52d3ed49f7cf72f6f0d8629476e
theplantpsychologist <69287005+theplantpsychologist@users.noreply.github.com>
2021-11-27 06:29:59 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+//const paper = require("./paper-core");
+
+//console.log("yoink");
+var yeesh = "yeesh";
+flaps = [100,200,300]
+circles = []
+point0 = new paper.Point(400,400);
+
+
+class Arc{
+	constructor(x,y,r){
+		this.x = x;
+		this.y = y;
+		this.r = r;
+	}
+}
+/*
+var x = new paper.Path.Circle({
+    center = new paper.Point(400,400),
+    radius: 70,
+    fillColor: 'green'
+})
+
+function update(){
+    for (var i=0;i<flaps.length;i++){
+        console.log(point0);
+        circle= new paper.Path.Circle(point0,flaps[i]);
+        circle.strokeColor = "purple";
+        circle.strokeWidth = 2;
+    }
+}
+*/
